d0ecc861ced517192e1b67a563c7a0d843c603af
Pavlo Malyshkin <pmalyshkin@gmail.com>
2025-05-11 23:19:12 +0300

    feat: router improvements; add 'create' route

--- src/router.ts
+++ src/router.ts
@@ -1,23 +1,43 @@
 import { IncomingMessage, ServerResponse } from "node:http"
 
 import logger from "@/middlewares/logger"
+
 import login from "@/api/login"
+import create from "@/api/create"
 
 interface IRouter {
-  [key: string]: (req: IncomingMessage, res: ServerResponse) => void
+  [key: string]: {
+    method: string
+    action: (req: IncomingMessage, res: ServerResponse) => void
+  }
 }
 
 export default function router(req: IncomingMessage, res: ServerResponse) {
   logger(req, res, () => {})
 
   const routes: IRouter = {
-    '/login': login,
+    '/login': {
+      method: 'POST',
+      action: login,
+    },
+    '/create': {
+      method: 'POST',
+      action: create,
+    },
   }
 
   if (!routes[req.url as string]) {
     res.statusCode = 404
+    res.write('404 not found')
     res.end()
     return
   }
-  routes[req.url as string](req, res)
+  if (req.method !== routes[req.url as string].method) {
+    res.statusCode = 405
+    res.write('405 method not allowed')
+    res.end()
+    return
+  }
+
+  routes[req.url as string].action(req, res)
 }
